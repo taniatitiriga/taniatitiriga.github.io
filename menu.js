792f251f11c06d38142d010653d4ec064e243801
taniatitiriga <tania.titiriga05@e-uvt.ro>
2024-05-08 12:50:48 +0300

    toggle sidebar

--- menu.js
+++ menu.js
@@ -1,7 +1,12 @@
 function toggleSidebar() {
     const sidebar = document.querySelector('aside');
     const toggleButton = document.getElementById('sidebar-toggle');
+    const articles = document.querySelectorAll('.arttool');
 
     sidebar.classList.toggle('show-sidebar');
     toggleButton.textContent = sidebar.classList.contains('show-sidebar') ? '<' : '>';
+
+    articles.forEach(article => {
+        article.style.marginLeft = sidebar.classList.contains('show-sidebar') ? 'calc(25% + 15px)' : '0';
+    });
 }
